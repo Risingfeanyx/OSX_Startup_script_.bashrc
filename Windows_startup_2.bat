7c5992119e9b7ec0dc675c8c993c570c577fb669
Risingfeanyx <35235934+Risingfeanyx@users.noreply.github.com>
2020-10-23 20:46:42 -0400

    Update Windows_startup_2.bat

--- Windows_startup_2.bat
+++ Windows_startup_2.bat
@@ -1,6 +1,6 @@
 choco feature enable -n allowGlobalConfirmation
 echo "This is where the fun begings. Installs commonly used programs. Edit batch file to add/remove. Good for a general catch-all list of programs."
-choco install steam firefox chrome libreoffice malwarebytes unchecky vlc -y 
+choco install steam firefox chrome libreoffice malwarebytes unchecky vlc office365proplus -y 
 choco upgrade all -y
 echo "Removes trace of install script and any unwanted programs"
 start /WAIT /MAX appwiz.cpl
